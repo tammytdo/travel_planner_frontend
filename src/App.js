8f013108a39dacb1a2f6633b76eaca7154417528
tammytdo11 <tammytdo11@gmail.com>
2023-11-07 10:13:37 -0800

    added city and month

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Form, Button, Container, Dropdown } from "react-bootstrap";
 
 function App() {
+  const [userFormData, setUserFormData] = useState({
+    city: "",
+    month: "",
+    months: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]
+  });
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+
+    const { name, value } = e.target;
+    setUserFormData({
+      ...userFormData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted city:", userFormData.city);
+    console.log("Submitted month:", userFormData.month);
+  };
+
+  console.log(userFormData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group controlId="user-city">
+            <Form.Label>Enter a City: </Form.Label>
+            <Form.Control
+              type="text"
+              name="city"
+              placeholder="city"
+              value={userFormData.city}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="select-month">
+            <Form.Label>Travel Month: </Form.Label>
+            <Form.Control 
+              as="select"
+              name="month"
+              value={userFormData.month}
+              onChange={handleInputChange}
+            >
+              <option value="" disabled>Select a month</option>
+              {userFormData.months.map((month, idx) => (
+                <option key={idx}>{month}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Search</Button>
+        </Form>
+      </Container>
     </div>
   );
 }
