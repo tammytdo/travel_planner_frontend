924d58d60919c84bb4f9c59e4c3e56edb3215726
tammytdo11 <tammytdo11@gmail.com>
2023-11-19 03:16:46 -0800

    basic rendering of data response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import React, { useState } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Card } from "react-bootstrap";
 import axios from "axios";
+import sampleResponse from "./sampleResponse.json";
 
 function App() {
   const [userFormData, setUserFormData] = useState({
@@ -21,6 +22,11 @@ function App() {
       "November",
       "December",
     ],
+    errorMsg: "",
+    attractions: [],
+    restaurants: [],
+    typical_weather: "",
+    upcoming_weather: [],
   });
 
   const handleInputChange = (e) => {
@@ -33,27 +39,55 @@ function App() {
     });
   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    fetchData();
-    console.log(e);
-  };
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   fetchData();
+  //   console.log(e);
+  // };
 
-  const fetchData = async () => {
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
     try {
       if (userFormData.city && userFormData.month) {
-        const apiEndpoint = `http://localhost:5000/getCityData?user_destination=${userFormData.city}&month=${userFormData.month}`;
-
-        const response = await axios.get(apiEndpoint); 
-
-        console.log(response.data);
+        // Instead of making an actual API call, use the imported sampleResponse
+        setUserFormData({
+          ...userFormData,
+          attractions: sampleResponse.attractions,
+          restaurants: sampleResponse.restaurants,
+          typical_weather: sampleResponse.typical_weather,
+          upcoming_weather: sampleResponse.upcoming_weather,
+        });
       } else {
         console.error("City and month are required");
       }
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error:", error);
     }
   };
+
+  // const fetchData = async () => {
+  //   try {
+  //     if (userFormData.city && userFormData.month) {
+  //       const apiEndpoint = `http://localhost:5000/getCityData?user_destination=${userFormData.city}&month=${userFormData.month}`;
+
+  //       const response = await axios.get(apiEndpoint);
+
+  //       setUserFormData({
+  //         ...userFormData,
+  //         attractions: response.data.attractions,
+  //         restaurants: response.data.restaurants,
+  //         typical_weather: response.data.typical_weather,
+  //         upcoming_weather: response.data.upcoming_weather,
+  //       });
+  //     } else {
+  //       console.error("City and month are required");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
+  console.log("userFormData:", userFormData);
 
   return (
     <div className="App">
@@ -89,6 +123,51 @@ function App() {
           </Form.Group>
           <Button type="submit">Search</Button>
         </Form>
+      </Container>
+
+      <Container>
+        <p id='typical_weather'>{userFormData.typical_weather}</p>
+      </Container>
+
+      <Container>
+        {userFormData.upcoming_weather.map((weatherDay, idx) => (
+          <Card id='upcoming_weather' key={`weatherDay-${idx}`}>
+            <Card.Body>
+              <Card.Text>{weatherDay.date}</Card.Text>
+              <p>Min Temp: {weatherDay.min_temp}</p>
+              <p>Max Temp: {weatherDay.max_temp}</p>
+              <p>Description: {weatherDay.description}</p>
+            </Card.Body>
+          </Card>
+        ))}
+      </Container>
+
+      <Container>
+        {userFormData.attractions.map((attraction, idx) => {
+          const [attractionName, attractionRating] = attraction.split(": "); 
+          return (
+            <Card id='attractions' key={`attraction-${idx}`}>
+              <Card.Body>
+                <Card.Title>{attractionName}</Card.Title>
+                <Card.Text>{attractionRating}</Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </Container>
+
+      <Container>
+        {userFormData.restaurants.map((restaurant, idx) => {
+          const [restaurantName, restaurantRating] = restaurant.split(": "); 
+          return (
+            <Card id='restaurants' key={`restaurant-${idx}`}>
+              <Card.Body>
+                <Card.Title>{restaurantName}</Card.Title>
+                <Card.Text>{restaurantRating}</Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        })}
       </Container>
     </div>
   );
