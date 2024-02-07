5f42b2dab8a80be122f1475bd7e884d8aab3725c
tammytdo11 <tammytdo11@gmail.com>
2024-02-07 00:46:34 -0800

    successful map image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import sampleResponse from "./sampleResponse.json"; // for testing
 import InputForm from "./components/InputForm/InputForm";
+import { Container, Row, Col } from "react-bootstrap"; // Importing Container, Row, Col from React Bootstrap
 import TypicalWeatherCard from "./components/Weather/TypicalWeatherCard";
 import Map from "./components/Map/Map";
 import WeatherCard from "./components/Weather/WeatherCard";
@@ -29,10 +30,8 @@ function App() {
     ],
   });
   const [cityData, setCityData] = useState({
-    // city: "",
     city: "Seattle", // Seattle for testing
     month: "",
-    // month: "Select a month", // January for testing
     lat: "",
     lon: "",
     response: {},
@@ -42,10 +41,6 @@ function App() {
     restaurants: [],
     upcomingWeather: [],
     mapUrl: "",
-    // typicalWeather: sampleResponse.typical_weather, // Sample for testing
-    // upcomingWeather: sampleResponse.upcoming_weather, // Sample for testing
-    // attractions: sampleResponse.attractions, // Sample for testing
-    // restaurants: sampleResponse.restaurants, // Sample for testing
   });
 
   const handleInputChange = (e) => {
@@ -56,86 +51,70 @@ function App() {
     });
   };
 
-  // testing
-  // const handleFormSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     if (cityData.city && cityData.month) {
-  //       // Instead of making an actual API call, use the imported sampleResponse
-  //       setCityData({
-  //         ...cityData,
-  //         response: sampleResponse,
-  //         lat: sampleResponse.lat,
-  //         lng: sampleResponse.lng,
-  //         attractions: sampleResponse.attractions,
-  //         restaurants: sampleResponse.restaurants,
-  //         typicalWeather: sampleResponse.typical_weather,
-  //         upcomingWeather: sampleResponse.upcoming_weather,
-  //         mapUrl: `https://www.google.com/maps/embed/v1/view?key=${apiKeyGoogle}&center=${sampleResponse.lat},${sampleResponse.lng}&zoom=15`
-  //       });
-  //     } else {
-  //       console.error("City and month are required");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
-
-  // Live API call
   const handleFormSubmit = (e) => {
     e.preventDefault();
     fetchData();
   };
 
-  // Live API call
   const fetchData = async () => {
-    try {
-      if (cityData.city && cityData.month) {
-        const apiEndpoint = `http://localhost:5000/getCityData?user_destination=${cityData.city}&month=${cityData.month}`;
+    if (cityData.city && cityData.month) {
+      const apiEndpoint = `http://localhost:5000/getCityData?user_destination=${cityData.city}&month=${cityData.month}`;
+      const response = await axios.get(apiEndpoint);
 
-        const response = await axios.get(apiEndpoint);
-        console.log('response lat', response.data.lat)
-
-        setCityData({
-          ...cityData,
-          lat: response.data.lat,
-          lng: response.data.lng,
-          attractions: response.data.attractions,
-          restaurants: response.data.restaurants,
-          typicalWeather: response.data.typical_weather,
-          upcomingWeather: response.data.upcoming_weather,
-          mapUrl: `https://www.google.com/maps/embed/v1/view?key=${apiKeyGoogle}&center=${response.data.lat},${response.data.lng}&zoom=15`,
-        });
-      } else {
-        console.error("City and month are required");
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
+      setCityData({
+        ...cityData,
+        lat: response.data.lat,
+        lon: response.data.lon,
+        attractions: response.data.attractions,
+        restaurants: response.data.restaurants,
+        typicalWeather: response.data.typical_weather,
+        upcomingWeather: response.data.upcoming_weather,
+        mapUrl: `https://www.google.com/maps/embed/v1/view?key=${apiKeyGoogle}&center=${response.data.lat},${response.data.lon}&zoom=15`,
+      });
     }
   };
 
   return (
-    <div className="App">
-      <InputForm
-        city={cityData.city}
-        month={cityData.month}
-        months={formData.months}
-        handleInputChange={handleInputChange}
-        handleFormSubmit={handleFormSubmit}
-      />
-
+    <Container className="App">
+      <Row>
+        <Col>
+          <InputForm
+            city={cityData.city}
+            month={cityData.month}
+            months={formData.months}
+            handleInputChange={handleInputChange}
+            handleFormSubmit={handleFormSubmit}
+          />
+        </Col>
+      </Row>
       {cityData.response && (
-        <div>
-          <TypicalWeatherCard typicalWeather={cityData.typicalWeather} />
-          <WeatherCard upcomingWeather={cityData.upcomingWeather} />
-          {/* edit to get attraction description and image */}
-          <AttractionCard attractions={cityData.attractions} />
-          {/* edit to get top 10 restaurants within 30 miles and image */}
-          <RestaurantCard restaurants={cityData.restaurants} />
-          <Map mapUrl={cityData.mapUrl} />
-        </div>
+        <Row>
+          <Col>
+            <TypicalWeatherCard typicalWeather={cityData.typicalWeather} />
+          </Col>
+          <Col>
+            <WeatherCard upcomingWeather={cityData.upcomingWeather} />
+          </Col>
+        </Row>
       )}
-    </div>
+      {cityData.response && (
+        <Row>
+          <Col>
+            <AttractionCard attractions={cityData.attractions} />
+          </Col>
+          <Col>
+            <RestaurantCard restaurants={cityData.restaurants} />
+          </Col>
+        </Row>
+      )}
+      {cityData.response && (
+        <Row>
+          <Col>
+            <Map mapUrl={cityData.mapUrl} />
+          </Col>
+        </Row>
+      )}
+    </Container>
   );
 }
 
